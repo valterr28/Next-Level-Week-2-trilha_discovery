d159aef21dbc769c37b709914fe85e9d5b88b4ff
Valter Rogério da Silva <40283319+valterr28@users.noreply.github.com>
2020-08-10 18:51:35 -0300

    quinto dia

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,5 +1,4 @@
 const subjects = [
-
     "Artes",
     "Biologia",
     "Ciências",
@@ -9,35 +8,32 @@ const subjects = [
     "História",
     "Matemática",
     "Portugês",
-    "Química",
-]
-
-const weekdays = [
-
-"Domingo",
-"Segunda-feira",
-"Terça-feira",
-"Quarta-feira",
-"Quinta-feira",
-"Sexta-feira",
-"Sábado",
-]
-
-
-function getSubject(subjectNumber) {
-    const postion = +subjectNumber -1
-    return subjects[postion]
-}
-
-function convertHoursToMinutes(time) {
-    const [hour, minutes] = time.split(":")
+    "Química"
+  ]
+  
+  const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+  ]
+  
+  function getSubjects(subjectNumber) {
+    const position = +subjectNumber -1
+    return subjects[position]
+  }
+  
+  function convertHoursToMinutes(time) {
+    const [ hour, minutes ] = time.split(":")
     return Number((hour * 60) + minutes)
-}
-
-module.exports = {
+  }
+  
+  module.exports = {
     subjects,
     weekdays,
-    getSubject,
+    getSubjects,
     convertHoursToMinutes
-
-}
+  }
